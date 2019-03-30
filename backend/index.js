53a2b3ba8059fea948a1980a19c770be199145da
Zachary Sy <zsy@nd.edu>
2019-03-29 22:21:22 -0400

    help me

--- backend/index.js
+++ backend/index.js
@@ -1,14 +1,16 @@
-const express = require('express')
-const app = express()
-
-const fs = require("fs")
-const port = 3000
+const express = require('express');
+const app = express();
+const fs = require("fs");
+const parse = require('parse');
+const port = 3000;
 
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
 
+// Routes
 app.get('/', (req, res)=>{
     fs.readFile("../frontend/index.html",'utf8',(err,data)=>{
-        res.send(data)
+        res.send(data);
     })
 })
+
