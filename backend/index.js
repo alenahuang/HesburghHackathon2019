abf205b0380400be4293d778bc5942542bdb7066
Zachary Sy <zsy@nd.edu>
2019-03-29 21:49:31 -0400

    express is working now

--- backend/index.js
+++ backend/index.js
@@ -8,8 +8,7 @@ const port = 3000
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
 
 app.get('/', (req, res)=>{
-
-    res.status(200);
-    res.send(data);
+    fs.readFile("../frontend/index.html",'utf8',(err,data)=>{
+        res.send(data)
+    })
 })
-
