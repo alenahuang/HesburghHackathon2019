069b7d0d2ddaeafdc8b96d2b9ce9a9e74c9b9295
Zachary Sy <zsy@nd.edu>
2019-03-30 11:06:26 -0400

    fixed stuff yay

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const app = express();
 const fs = require("fs");
-const parse = require('parse/node');
+const Parse = require('Parse/node');
 const port = 3000;
 
-parse.initialize('1UL4z4XXj5lcw7FqCZQDi9AEY9oBaCLDU1hlLsDI', 'wCS800zhokit6qVVcY7dvJdunL0yAuIDKe2em9sV');
-parse.serverURL = 'https://parseapi.back4app.com/';
+Parse.initialize('1UL4z4XXj5lcw7FqCZQDi9AEY9oBaCLDU1hlLsDI', 'wCS800zhokit6qVVcY7dvJdunL0yAuIDKe2em9sV');
+Parse.serverURL = 'https://parseapi.back4app.com/';
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
 app.use(express.static('../frontend'))
@@ -21,9 +21,20 @@ app.get('/asdf', (req, res)=>{
     res.send("LOLSBEAUTIFUL")
 })
 
-app.get('/test', (req, res) => {
-    var Advice = parse.Object.extend('Advice');
-    var query = new parse.Query(Advice);
+app.get('/advices', (req, res) => {
+    var Advice = Parse.Object.extend('Advice');
+    var query = new Parse.Query(Advice);
+    query.equalTo('section',req.params["category"])
+    query.find()
+        .then(data => {
+           res.send(data);
+        });
+})
+
+app.get('/reviews', (req, res) => {
+    var Reviews = Parse.Object.extend('Reviews');
+    var query = new Parse.Query(Reviews);
+    query.equalTo('section',req.params["category"])
     query.find()
         .then(data => {
            res.send(data);
