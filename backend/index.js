9b273a55352990748540857078b1ded6d3599d03
Patrick Soga <psoga117@gmail.com>
2019-03-29 22:33:20 -0400

    Parse stuff

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const app = express();
 const fs = require("fs");
-const parse = require('parse');
+const parse = require('parse/node');
 const port = 3000;
 
+parse.initialize('1UL4z4XXj5lcw7FqCZQDi9AEY9oBaCLDU1hlLsDI', 'wCS800zhokit6qVVcY7dvJdunL0yAuIDKe2em9sV');
+parse.serverURL = 'https://parseapi.back4app.com/';
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
 
