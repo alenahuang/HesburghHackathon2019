3a31678c78e8fa1f2615bbc1e5afe50ac8c20935
Zachary Sy <zsy@nd.edu>
2019-03-30 21:22:32 -0400

    a

--- frontend/app.js
+++ frontend/app.js
@@ -45,10 +45,21 @@ app.controller('userCreationController', ["$scope", "$http", function($scope, $h
 
 }]);
 
-app.controller('reviewsController', ["$scope", "$http", function($scope, $http) {
+app.controller('reviewsController', ["$scope", "$http","orderByFilter", function($scope, $http, orderBy) {
     $http.get('/reviews',{"params":{"category":"food"}}).then(function (res) {
         $scope.reviews = res.data;
     });
+
+    $scope.upvotes = function(){
+        $scope.reviews = orderBy($scope.reviews, "upvotes", true);
+    }
+     $scope.stars = function(most){
+         if(most){
+              $scope.reviews = orderBy($scope.reviews, "stars", true);
+         }else{
+              $scope.reviews = orderBy($scope.reviews, "stars", false);
+         }
+    }
 }]);
 
 app.controller('adviceController', ["$scope", "$http", function($scope, $http) {
